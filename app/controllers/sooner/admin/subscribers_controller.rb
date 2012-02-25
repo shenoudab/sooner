aa5eb2324738388808d9bee8e62548a4ddad26fd
Shenouda Bertel <ShenoudaB@gmail.com>
2012-02-26 01:21:10 +0200

    Update in Views

--- app/controllers/sooner/admin/subscribers_controller.rb
+++ app/controllers/sooner/admin/subscribers_controller.rb
@@ -79,7 +79,7 @@ module Sooner
       @subscriber.destroy
 
       respond_to do |format|
-        format.html { redirect_to subscribers_url }
+        format.html { redirect_to admin_url }
         format.json { head :ok }
       end
     end
